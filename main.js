f99dbf7e50cd0cc0717b37cff76d792b349f9639
AlessioGugielmino <guglielmino.alessio@virgilio.it>
2023-07-19 16:36:34 +0200

    feat:complete

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
-const age = prompt("inserisci la tua età");
-const km = prompt("inserisci quanti chilometri vuoi percorrere");
+let age = prompt("inserisci la tua età");
+let km = prompt("inserisci quanti chilometri vuoi percorrere");
+let price = km * 0.21;
+
+if (age < 18) {
+  price = price - (price / 100) * 20;
+} else if (age > 65) {
+  price = price - (price / 100) * 40;
+}
+
+console.log(price);
+document.getElementById("price").innerHTML =
+  "il prezzo del biglietto è: €" + price.toFixed(2);
